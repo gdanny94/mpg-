cbbdb4b174c7d36473ce5fb1badcc6940823c83a
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-11-05 17:59:42 -0500

    codewars

---
+++ main.js
@@ -0,0 +1,9 @@
+function zeroFuel(distanceToPump, mpg, fuelLeft) {
+    
+    
+    return (mpg * fuelLeft) >= distanceToPump 
+
+  }
+
+
+  //dont need terniary since comparison automatically makes it true or false. 
